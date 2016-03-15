a6847e0789bb09ecb241715777133ddb8eb78169
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-03-15 09:39:37 -0500

    cambio .jmx experimento 1

--- SistemaDeTsunamis/mueblesdelosalpes/mueblesdelosalpes.backend/target/generated-sources/annotations/sistemaAlerta/entity/Boletin_.java
+++ SistemaDeTsunamis/mueblesdelosalpes/mueblesdelosalpes.backend/target/generated-sources/annotations/sistemaAlerta/entity/Boletin_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-15T08:41:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-15T09:30:47")
 @StaticMetamodel(Boletin.class)
 public class Boletin_ { 
 
--- SistemaDeTsunamis/mueblesdelosalpes/mueblesdelosalpes.backend/target/generated-sources/annotations/sistemaAlerta/entity/Sensor_.java
+++ SistemaDeTsunamis/mueblesdelosalpes/mueblesdelosalpes.backend/target/generated-sources/annotations/sistemaAlerta/entity/Sensor_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import sistemaAlerta.entity.Parametro;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-15T08:41:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-15T09:30:47")
 @StaticMetamodel(Sensor.class)
 public class Sensor_ { 
 
